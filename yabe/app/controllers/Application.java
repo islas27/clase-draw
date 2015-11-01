1e55fa062975c3190e6d4cbc2a4e979ec0f4e738
Jonathan Islas <islas.27@gmail.com>
2015-10-31 22:16:03 -0700

    Se agrega funcionalidad de Tags y Captcha
    Commit que corresponde con las actividades de la sección 5 y 6 del tutorial de Play! Framework
    -- Actualización de datos iniciales en initial-data.yml
    -- Se agrega las rutas y acciones para captcha y las tags
    -- Se crea el modelo que le da soporte a las tags
    -- Se añade la vista listTagged.html
    -- Actualización a modelos y acciones para soportar tags

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -4,8 +4,9 @@ import play.*;
 import play.mvc.*;
 import play.data.validation.*;
 import java.util.*;
-
+import play.libs.*;
 import models.*;
+import play.cache.*;
 
 public class Application extends Controller {
     
@@ -25,17 +26,38 @@ public class Application extends Controller {
     
     public static void show(Long id) {
         Post post = Post.findById(id);
-        render(post);
+        String randomID = Codec.UUID();
+        render(post, randomID);
     }
     
-    public static void postComment(Long postId, @Required String author, @Required String content) {
-        Post post = Post.findById(postId);
-        if (validation.hasErrors()) {
-            render("Application/show.html", post);
-        }
-        post.addComment(author, content);
-        flash.success("Thanks for posting %s", author);
-        show(postId);
+    public static void postComment(
+        Long postId, 
+        @Required(message="Author is required") String author, 
+        @Required(message="A message is required") String content, 
+        @Required(message="Please type the code") String code, 
+        String randomID){
+            Post post = Post.findById(postId);
+            validation.equals(code, Cache.get(randomID))
+                .message("Invalid code. Please type it again");
+            if(validation.hasErrors()) {
+                render("Application/show.html", post, randomID);
+            }
+            post.addComment(author, content);
+            flash.success("Thanks for posting %s", author);
+            Cache.delete(randomID);
+            show(postId);
+    }
+    
+    public static void captcha(String id){
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
+    
+    public static void listTagged(String tag) {
+        List<Post> posts = Post.findTaggedWith(tag);
+        render(tag, posts);
     }
 
 }
